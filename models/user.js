f9110560de7df04cdc210cfdc17e35eb7b07617a
iamcristos <nmereginivincent@gmail.com>
2019-01-02 17:50:00 +0000

    hashing users password update

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose= require('mongoose');
 const validator= require('validator');
 const jwt= require('jsonwebtoken');
-const _= require('lodash')
+const _= require('lodash');
+const bcrypt= require('bcryptjs')
 
 let userSchema= mongoose.Schema({
     email:{
@@ -69,7 +70,22 @@ userSchema.statics.findByToken= function(token){
 
     })
 
-}
+};
+
+userSchema.pre('save', function(next){
+    let user= this;
+
+    if(user.isModified('password')) {
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password=hash;
+            });
+            next()
+        })
+    } else {
+        next()
+    }
+})
 
 let User= mongoose.model('User', userSchema);
 
